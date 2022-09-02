3f690c67c3153d2532d597b370454db0741cb1fa
JaeySen <longhientranjjj@gmail.com>
2022-09-02 18:54:01 +0700

    add ts-jest

--- dest/index.js
+++ dest/index.js
@@ -1,14 +1,14 @@
 "use strict";
-exports.__esModule = true;
-var uuid_1 = require("uuid");
-var list = document.querySelector("#list");
-var form = document.querySelector("#new-task-form");
-var input = document.querySelector("#new-task-title");
-form === null || form === void 0 ? void 0 : form.addEventListener("submit", function (e) {
+Object.defineProperty(exports, "__esModule", { value: true });
+const uuid_1 = require("uuid");
+const list = document.querySelector("#list");
+const form = document.querySelector("#new-task-form");
+const input = document.querySelector("#new-task-title");
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", e => {
     e.preventDefault();
     if ((input === null || input === void 0 ? void 0 : input.value) == "" || (input === null || input === void 0 ? void 0 : input.value) == null)
         return;
-    var task = {
+    const task = {
         id: (0, uuid_1.v4)(),
         title: input.value,
         completed: false,
